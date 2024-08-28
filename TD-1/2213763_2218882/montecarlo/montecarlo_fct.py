41ca0901c70b7c2dcda0d7b2dce31584ed28a1af
sebTF <tardiffrancoeur@hotmail.ca>
2024-08-28 11:19:20 -0400

    ajout monte carlos

--- TD-1/2213763_2218882/montecarlo/montecarlo_fct.py
+++ TD-1/2213763_2218882/montecarlo/montecarlo_fct.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def genXY(n):
     """Fonction de génération de points aléatoires
     
@@ -14,8 +15,23 @@ def genXY(n):
     assert isinstance(n,int) or isinstance(n,np.int32), "n pas du bon type."
     
     ### Fonction à écrire
-    
-    return ### Valeurs retournées
+    position_x = []
+    position_y = []
+    i=0
+    r=1 
+    while i < n :
+        x = np.random.uniform(-r,r)
+        position_x.append(x)
+        i+=1
+    i=0
+    while i < n :
+        y = np.random.uniform(-r,r)
+        position_y.append(y)
+        i+=1
+  
+   
+    return position_x, position_y
+
 
 def monte_carlo(x,y):
     """Fonction calculant pi par la méthode de Monte Carlo
@@ -30,5 +46,38 @@ def monte_carlo(x,y):
     assert len(x)==len(y), "Grandeurs des vecteurs ne concordent pas."
     
     ### Fonction à écrire
+    r=1
+    x2 = np.square(x)
+    y2 = np.square(y)
+    xy = np.add(x2,y2)
+    N = len(xy)
+    Nc= 0
     
-    return ### Valeur retournée
+    Nc = [i for i in xy if i <= 1]
+   
+    Pi = float((len(Nc)/N)*4)
+            
+    
+    return Pi
+
+pts_x = np.linspace(100, 10000+1, 100,dtype= int)
+
+pts_erreur = np.empty(pts_x.shape)
+
+for i in range(len(pts_x)) :
+    
+    xy = genXY(pts_x[i])
+    erreur_absolue = abs(np.pi - monte_carlo(xy[0], xy[1]))
+    pts_erreur[i] = erreur_absolue
+
+plt.plot(pts_x,pts_erreur)   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
