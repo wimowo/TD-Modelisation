35fceff5ec9bf2fe4dade99a254601cff604629c
William <morin.william.4@gmail.com>
2024-08-28 10:18:17 -0400

    Complete serie harmonique

--- TD-1/2213763_2218882/algorithmie/algorithmie_analyse.py
+++ TD-1/2213763_2218882/algorithmie/algorithmie_analyse.py
@@ -9,14 +9,21 @@ except:
 
 import algorithmie_corr
 
-
 # Série harmonique
 
+x_axis = np.linspace(50, 100, 51, dtype=int)
+y_axis = np.empty(x_axis.shape)
 
+for i in range(x_axis.size):
+    y_axis[i] = serie_harmonique(x_axis[i])
+
+plt.plot(x_axis, y_axis)
+plt.legend(['Harmonique'])
+plt.show()
 
 # Factoriel
 
-
+#print(np.math.factorial(50))
 
 # Correction
-pytest.main(['-q', '--tb=long', 'algorithmie_corr.py'])
+#pytest.main(['-q', '--tb=long', 'algorithmie_corr.py'])
--- TD-1/2213763_2218882/algorithmie/algorithmie_fct.py
+++ TD-1/2213763_2218882/algorithmie/algorithmie_fct.py
@@ -1,7 +1,6 @@
 #Importation des modules
 import numpy as np
 
-
 # Fonction de la serie harmonique
 def serie_harmonique(N):
     """Fonction qui calcule la série harmonique en sommant les N premiers termes
@@ -9,9 +8,12 @@ def serie_harmonique(N):
     Sortie:
         - Un nombre flottant contenant la valeur de la série
     """
+    sum = 0
+
+    for i in range(N):
+        sum += 1/(i+1)
     
-    
-    return 0
+    return sum
 
 # Fonction de la fonction factorielle
 def factoriel(k):
@@ -20,6 +22,8 @@ def factoriel(k):
     Sortie:
         - Un nombre entier contenant le factoriel de k
     """
+    factorial = 1
+    for i in range(1, k+1):
+        factorial *= i
 
-
-    return 0
+    return factorial
