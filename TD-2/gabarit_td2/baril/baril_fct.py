d73277036aba183505f1b05bd0546f1917ec32a9
Wimowo <51500894+wimowo@users.noreply.github.com>
2024-09-22 14:01:15 -0400

    TD3 complété

--- TD-2/gabarit_td2/baril/baril_fct.py
+++ TD-2/gabarit_td2/baril/baril_fct.py
@@ -1,8 +1,9 @@
 # Importation des modules
 import numpy as np
 
-#-------------------------------------Fonction diff1()-----------------------#
-def diff1(h,dt):
+
+# -------------------------------------Fonction diff1()-----------------------#
+def diff1(h, dt):
     """Fonction qui calcule la dérivée première
     
     Entrées:
@@ -12,13 +13,12 @@ def diff1(h,dt):
     Sortie:
       - Vecteur (array) contenant les valeurs numériques de la dérivée première
     """
-    
-    # Fonction à écrire
-    deriv1 = np.gradient(h,dt, edge_order=2)
-    return deriv1
 
-#-------------------------------------Fonction diff2()-----------------------#
-def diff2(h,dt):
+    return np.gradient(h, dt, edge_order=2)
+
+
+# -------------------------------------Fonction diff2()-----------------------#
+def diff2(h, dt):
     """Fonction qui calcule la dérivée seconde
     
     Entrées:
@@ -28,22 +28,23 @@ def diff2(h,dt):
     Sortie:
       - Vecteur (array) contenant les valeurs numériques de la dérivée seconde
     """
-    
+
     # Fonction à écrire
-   
-    deriv2 = np.empty(len(h),dtype=float)
-    i=0
+
+    deriv2 = np.empty(len(h), dtype=float)
+    i = 0
     while i < len(h):
         if i == 0:
-            deriv2[i] = (h[i+2]-2*h[i+1]+h[i])/(dt**2)
-        elif i == (len(h)-1):
-            deriv2[i] = (h[i]-2*h[i-1]+h[i-2])/(dt**2)
+            deriv2[i] = (h[i + 2] - 2 * h[i + 1] + h[i]) / (dt ** 2)
+        elif i == (len(h) - 1):
+            deriv2[i] = (h[i] - 2 * h[i - 1] + h[i - 2]) / (dt ** 2)
         else:
-            deriv2[i] = (h[i+1]-2*h[i]+h[i-1])/(dt**2)
-        i+=1
+            deriv2[i] = (h[i + 1] - 2 * h[i] + h[i - 1]) / (dt ** 2)
+        i += 1
     return deriv2
 
-#------------------------------Fonction acceleration()-----------------------#
+
+# ------------------------------Fonction acceleration()-----------------------#
 def acceleration(cst):
     """Fonction qui calcule l'accélération théorique de la surface libre
     
@@ -57,11 +58,12 @@ def acceleration(cst):
     Sortie:
       - Valeur numérique théorique de l'accélération de la surface libre [float]
     """
-        
-    return ((cst.gamma*(cst.rv/cst.rc)**2)**2)*cst.g
 
-#-----------------------------------Fonction vitesse()-----------------------#
-def vitesse(h,cst):
+    return ((cst.gamma * (cst.rv / cst.rc) ** 2) ** 2) * cst.g
+
+
+# -----------------------------------Fonction vitesse()-----------------------#
+def vitesse(h, cst):
     """Fonction qui calcule la vitesse théorique de la surface libre
     
     Entrées:
@@ -75,29 +77,10 @@ def vitesse(h,cst):
     Sortie:
       - Vecteur de valeurs numériques théoriques de la vitesse de la surface libre [array]
     """
-    
+
     v = np.empty(len(h))
-    
+
     for i in np.arange(len(h)):
-        v[i] = -cst.gamma * ((cst.rv/cst.rc)**2) * np.sqrt(2 * cst.g * h[i])
-    
+        v[i] = -cst.gamma * ((cst.rv / cst.rc) ** 2) * np.sqrt(2 * cst.g * h[i])
+
     return v
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TD-3/2213763_2218882/tige/tige_analyse.py
+++ TD-3/2213763_2218882/tige/tige_analyse.py
@@ -8,25 +8,27 @@ try:
 except:
     pass
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Code principal pour l'analyse des résultats
 # Il faudra faire appel aux fonctions programmées dans tige_fct.py afin de
 # calculer l'intégrale d'une fonction selon la quadrature de Gauss. Ensuite,
 # il faudra évaluer l'erreur par rapport à la valeur analytique et afficher
 # un graphique de cette erreur selon le nombre de points.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#%% Calcul de l'intégrale numérique
-n = np.array([1,2,3,4,5])
+image_dir = "../../images/"
+
+# %% Calcul de l'intégrale numérique
+n = np.array([1, 2, 3, 4, 5])
 integrale = np.empty(len(n))
 
 for i in range(len(n)):
-    integrale[i] = gauss(0, np.pi/2, n[i])
-print(np.stack((n,integrale)))
+    integrale[i] = gauss(0, np.pi / 2, n[i])
+print(np.stack((n, integrale)))
 # Affichage en tableau
 
 
-#%% Calcul de l'erreur commise
+# %% Calcul de l'erreur commise
 t = 0.99888139
 erreur = np.abs(integrale - t)
 
@@ -36,11 +38,11 @@ plt.plot(n, erreur)
 plt.xlabel("Nombre de point")
 plt.ylabel("Erreur")
 plt.title("Évolution de l'erreur en fonction du nombre de point")
-plt.xticks(np.arange(0,6,1))
+plt.xticks(np.arange(0, 6, 1))
 plt.grid()
-
+plt.savefig(image_dir + "erreur.png")
 
 plt.show()
 
-#%% Correction
+# %% Correction
 pytest.main(['-q', '--tb=long', 'tige_corr.py'])
