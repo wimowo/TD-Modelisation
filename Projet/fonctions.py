6ba81ae2331f31fd71b415b620e1f0612d9d54fe
sebTF <tardiffrancoeur@hotmail.ca>
2024-11-23 15:46:41 -0500

    Tentative de résolution
    Ca fonctionne jusqu'au calcul du résidu qui est botché un peu j'ai essayé de le rentrer dans Newton mais ca donne une matrice singuliere... probablement un probleme avec les conditions initiales nulle ou négative?

--- Projet/fonctions.py
+++ Projet/fonctions.py
@@ -11,7 +11,7 @@ def Reynolds(Q) :
     return (4*rho*Q)/(np.pi*mu*D)
 
 def Cr(Q):
-    return (-2*2**0.5)/(3.83*Reynolds(Q)**0.105)*(np.log10(np.exp(1)/(3.7*D))+1.78/Reynolds(Q))
+    return (-2*2**0.5)/(3.83*Reynolds(Q)**0.105)*(np.log10(np.exp(1)/(3.7*D)+1.78/Reynolds(Q)))
 
 def Resistance(Q) :
     return L/ (944.62*Cr(Q)**1.8099*D**4.8099)
